370597b4ee7d673b46754006970cb63c44ffd349
TienLien <120458415+iamtien-cmd@users.noreply.github.com>
2024-10-30 10:05:43 +0700

    CRUD1

--- BTWEB30-10-2024/src/main/java/vn/iotstar/demo/DemoThymeleafApplication.java
+++ BTWEB30-10-2024/src/main/java/vn/iotstar/demo/DemoThymeleafApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoThymeleafApplication {
+public class DemoSitemeshThymeleafApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoThymeleafApplication.class, args);
+		SpringApplication.run(DemoSitemeshThymeleafApplication.class, args);
 	}
 
 }
